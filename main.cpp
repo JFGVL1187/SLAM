0803b6d53bc64e825910b3d66722b28451271dfe
JFGVL1187 <jiahao_ren@alumni.brown.edu>
2023-09-02 17:57:02 -0700

    Learn eigen 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "libHelloSLAM.h"
 #include <ctime>
-#include <Eigen/Core>
+//eigen lib's header folder is in /usr/include/eigen3
+#include <Eigen/Core> 
 #include <Eigen/Dense>
 using namespace Eigen;
 using namespace std;
@@ -10,7 +11,23 @@ int main(int argc, char** argv){
     cout << "Hello, SLAM" << endl;
     printHello();
 
-    Matrix<float, 2, 3> matrix_23;
+    Matrix<float, 2, 3> matrix_23f;
+    matrix_23f << 1,2,3,4,5,6;
+    cout << "matrix_23f: \n" << matrix_23f << endl;
+    cout << matrix_23f(0,0) << endl;
+
+    Vector3d v_3d; //Matrix<double, 3, 1> vd_3d;
+    v_3d << 3,2,1;
+    Matrix<double,2,1> result = matrix_23f.cast<double>() * v_3d;
+    cout << "matrix_23f * v_3d: \n" << result.transpose() << endl;
+
+    Matrix3d matrix_33 = Matrix3d::Zero(); //Matrix<double, 3, 3>
+    Matrix<double, Dynamic, Dynamic> matrix_dynamic;
+    MatrixXd matrix_x;
+
+    
+
+
 
     return 0;
 }
